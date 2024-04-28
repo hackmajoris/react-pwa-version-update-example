d40bda36392d453489cb555f177618c41d52fda3
Alex Ilies <alex.ilies@dot-core.com>
2024-04-28 15:14:08 +0300

    chore: version change, try1

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function App({callback}) {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Try1
         </p>
         <a
           className="App-link"
