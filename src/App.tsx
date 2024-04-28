25dae1c7c4a68c64e2471f974cccad22fde963d7
Alex Ilies <alex.ilies@dot-core.com>
2024-04-28 19:04:31 +0300

    try: service worker config: try 26

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Snackbar} from '@mui/material';
 import propTypes from 'prop-types';
 import {useServiceWorker} from "./userServiceWorker";
 
 function App() {
 
-  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
-  const [snackbarMessage, setSnackbarMessage] = React.useState('');
-
-  const { waitingWorker, showReload, reloadPage } = useServiceWorker();
-// decides when to show the toast
-  useEffect(() => {
-    if (showReload && waitingWorker) {
-      console.log('service worker update waisting');
-      setSnackbarMessage('A new version is available: exit the app to update');
-      setSnackbarOpen(true);
-
-    }
-  }, [waitingWorker, showReload, reloadPage]);
-
+  useServiceWorker()
   return (
     <div className="App">
       <header className="App-header">
@@ -39,20 +25,7 @@ function App() {
         </a>
       </header>
 
-      <Snackbar
-          open={snackbarOpen}
-          autoHideDuration={60000}
-          message={snackbarMessage}
-          action={
-            <Button color="secondary" size="small" onClick={reloadPage}>
-              Refresh
-            </Button>
-          }
-          onClose={() => setSnackbarOpen(false)}
-      />
-
     </div>
-
 
   );
 
