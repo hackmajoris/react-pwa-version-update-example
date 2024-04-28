760b8be5536c0aef4f00a26e96e4af41c5c04455
Alex Ilies <alex.ilies@dot-core.com>
2024-04-28 18:30:16 +0300

    try: service worker config: try 17

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          Try 16
+          Try 17
         </p>
         <a
           className="App-link"
--- src/userServiceWorker.ts
+++ src/userServiceWorker.ts
@@ -8,9 +8,21 @@ export const useServiceWorker = () => {
     // updates. this function is a callback
     // to the actual service worker
     // registration onUpdate.
-    const onSWUpdate = useCallback((registration: ServiceWorkerRegistration) => {
+    const onSWUpdate = useCallback(async (registration: ServiceWorkerRegistration) => {
+        // We want to run this code only if we detect a new service worker is
+        // waiting to be activated.
+        // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+        if (registration && registration.waiting) {
+            await registration.unregister();
+            // Makes Workbox call skipWaiting()
+            registration.waiting.postMessage({type: 'SKIP_WAITING'});
+            // Once the service worker is unregistered, we can reload the page to let
+            // the browser download a fresh copy of our app (invalidating the cache)
+            window.location.reload();
+        }
         setShowReload(true);
         setWaitingWorker(registration.waiting);
+
     }, []);
     // simply put, this tells the service
     // worker to skip the waiting phase and then reloads the page
