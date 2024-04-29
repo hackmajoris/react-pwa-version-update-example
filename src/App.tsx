5b9fe9186f4e2cf55cdf943164b0e9249fb9407d
Alex Ilies <alex.ilies@dot-core.com>
2024-04-29 21:09:35 +0300

    try: service worker config

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
-                    Try  71
+                    Try  72
                 </p>
                 <a
                     className="App-link"
